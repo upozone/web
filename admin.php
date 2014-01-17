52380893fe99abd2590aed90bbc7f4641efdf83d
upozone <upozone@qq.com>
2014-01-17 22:15:12 +0800

    建立web
    index
    admin

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php
+	define("APP_PATH","admin/");
+	define("DIR_SAFE",TRUE);
+	define("DEBUG",TRUE);
+	include"../hdphp/hdphp/hdphp.php";
+
+
+
+
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+	define("APP_PATH","web/");
+	define("DEBUG",TRUE);
+	define("DIR_SAFE",TRUE);
+	include"../hdphp/hdphp/hdphp.php";
+
+
+
+
